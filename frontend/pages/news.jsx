4424d9053eb88a968ac739ad9b39f1154254ee46
Murthy Thevar <152300792+MurXGen@users.noreply.github.com>
2025-10-23 10:18:36 +0530

    Coming soon pages

---
+++ frontend/pages/news.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import TickerAnalysis from "./TicketAnalysis";
+
+const News = ({ trades }) => {
+  return (
+    <div>
+      <h2>Coming soon...</h2>
+    </div>
+  );
+};
+
+export default News;
---
+++ frontend/pages/share-trades.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import TickerAnalysis from "./TicketAnalysis";
+
+const ShareTrades = ({ trades }) => {
+  return (
+    <div>
+      <h2>Coming soon...</h2>
+    </div>
+  );
+};
+
+export default ShareTrades;
