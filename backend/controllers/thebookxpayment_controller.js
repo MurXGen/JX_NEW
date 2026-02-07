dcc6a601329fb32689fa4bfb7db290b9cf3bac5b
MurXGen <murthyofficial3@gmail.com>
2026-02-08 01:13:27 +0530

    Url update for sandbox

--- backend/controllers/thebookxpayment_controller.js
+++ backend/controllers/thebookxpayment_controller.js
@@ -9,8 +9,12 @@ exports.createOrder = async (req, res) => {
   try {
     const { amount, city, pincode, address, cartItems } = req.body;
 
-    if (!amount) {
-      return res.status(400).json({ message: "Amount is required" });
+    console.log("Incoming body:", req.body);
+
+    if (!MERCHANT_ID || !SALT_KEY || !SALT_INDEX) {
+      return res.status(500).json({
+        message: "PhonePe env variables missing",
+      });
     }
 
     const transactionId = "TXN_" + Date.now();
@@ -19,10 +23,10 @@ exports.createOrder = async (req, res) => {
       merchantId: MERCHANT_ID,
       merchantTransactionId: transactionId,
       merchantUserId: "USER_" + Date.now(),
-      amount: amount * 100, // paise
+      amount: amount * 100,
       redirectUrl: "https://thebookx.in/payment-success",
       redirectMode: "POST",
-      callbackUrl: "https://api.journalx.app/api/payments/webhook",
+      callbackUrl: "https://api.journalx.app/api/thebooks/payments/webhook",
       paymentInstrument: {
         type: "PAY_PAGE",
       },
@@ -40,30 +44,42 @@ exports.createOrder = async (req, res) => {
       "###" +
       SALT_INDEX;
 
+    console.log("Sending to PhonePe...");
+
     const response = await axios.post(
-      "https://api.phonepe.com/apis/hermes/pg/v1/pay",
+      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay",
       { request: payloadBase64 },
       {
         headers: {
           "Content-Type": "application/json",
           "X-VERIFY": checksum,
         },
+        timeout: 15000,
       },
     );
 
-    return res.status(200).json({
-      paymentUrl: response.data.data.instrumentResponse.redirectInfo.url,
+    console.log("PhonePe response:", response.data);
+
+    const paymentUrl =
+      response.data?.data?.instrumentResponse?.redirectInfo?.url;
+
+    if (!paymentUrl) {
+      return res.status(500).json({
+        message: "PhonePe did not return payment URL",
+        response: response.data,
+      });
+    }
+
+    res.json({
+      paymentUrl,
       transactionId,
     });
   } catch (error) {
-    console.error(
-      "PhonePe create order error:",
-      error?.response?.data || error,
-    );
+    console.error("PhonePe error:", error.response?.data || error.message);
 
-    return res.status(500).json({
+    res.status(500).json({
       message: "Payment initiation failed",
-      error: error?.response?.data || error.message,
+      error: error.response?.data || error.message,
     });
   }
 };
