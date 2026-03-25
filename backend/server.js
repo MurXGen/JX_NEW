9d49e7ba38c6f4d47a796217ee64eed650ce1e84
MurXGen <murthyofficial3@gmail.com>
2026-03-26 00:35:04 +0530

    Remove limiter

--- backend/server.js
+++ backend/server.js
@@ -34,37 +34,19 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
 ];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true); // allow Postman/curl
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization", "x-trade-id"],
-//   }),
-// );
-
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin) || origin.includes("thebookx.in")) {
-        return callback(null, true);
+      if (!origin) return callback(null, true); // allow Postman/curl
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
       }
-
-      console.log("❌ Blocked by CORS:", origin);
-
-      // ❗ DO NOT THROW ERROR
-      return callback(null, false);
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-trade-id"],
   }),
 );
 
